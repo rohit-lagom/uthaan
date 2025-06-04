16033bce5a4b0ba098ff4b34bcdd118c2c4232ea
rohit-lagom <rohit@lagomchain.com>
2025-06-04 12:57:40 +0530

    fix (App): reraange the sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
-import Introduction from "./components/Introduction/Introduction";
 import InfoBanner from "./components/InfoBanner/InfoBanner";
+import Introduction from "./components/Introduction/Introduction";
+import Benefits from "./components/Benefits/Benefits";
+import Platform from "./components/Platform/Platform";
 import OurServices from "./components/OurServices/OurServices";
-import FAQ from "./components/FAQ/FAQ";
 import RecentWorks from "./components/RecentWorks/RecentWorks";
 import Testimonials from "./components/Testimonials/Testimonials";
+import FAQ from "./components/FAQ/FAQ";
 import Blogs from "./components/Blogs/Blogs";
+import CTA from "./components/CTA/CTA";
 import QuoteFooter from "./components/Footer/QuoteFooter";
 import Footer from "./components/Footer/Footer";
-import CTA from "./components/CTA/CTA";
-import Platform from "./components/Platform/Platform";
-import Benefits from "./components/Benefits/Benefits";
 
 const App = () => {
   return (
     <div>
       <Hero />
-      <Introduction/>
-      <InfoBanner/>
-      <OurServices/>
-      <Benefits/>
-      <Platform/>
-      <FAQ/>
-      <RecentWorks/>
-      <Testimonials/>
-      <Blogs/>
-      <CTA/>
-      {/* <QuoteFooter/> */}
-      <Footer/>
+      <Introduction />
+      <InfoBanner />
+      <Benefits />
+      <Platform />
+      <OurServices />
+      <RecentWorks />
+      <Testimonials />
+      <FAQ />
+      <Blogs />
+      <CTA />
+      {/* <QuoteFooter /> */}
+      <Footer />
     </div>
   );
 };
